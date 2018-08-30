e8abd48c87045b3652097257959654eb5eba690f
Nick Miller <nick@nmiller.info>
2018-08-30 01:01:36 -0400

    Cleanup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-ARGC=$#
-ARGV=$*
-OS="$(uname -a | xargs -n1 | head -n1)"
 # Exit with a message if $1 is nonzero.
 # $1: error code
 # $2: message
@@ -16,9 +13,8 @@ function exit_if() {
 # Ensure SIGINT exits entire script instead of the current task:
 trap exit SIGINT
 
-if [ "$OS" != "Darwin" ]; then
-	echo "Script can only be run on macOS" >&2
-	exit 1
+if [ "$(uname -a | xargs -n1 | head -n1)" != "Darwin" ]; then
+	exit_if 1 "Script can only be run on macOS"
 fi
 
 # Defaults:
@@ -31,10 +27,10 @@ killall Dock 2>/dev/null && killall Finder 2>/dev/null
 
 # Install Command Line Tools for Xcode:
 TOPIC="Command Line Tools for Xcode"
-CLTOOLS=$(pkgutil --pkg-info=com.apple.pkg.CLTools_Executables)
+CLTOOLS="$(pkgutil --pkg-info=com.apple.pkg.CLTools_Executables)"
 
 if [ $? -eq 1 ]; then
-	echo "Installing $TOPIC..."
+	echo "Installing ${TOPIC}..."
 	touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
 	local prod="$(
 		softwareupdate -l
@@ -43,8 +39,8 @@ if [ $? -eq 1 ]; then
 			| sed -e 's/^ *//'
 			| tr -d '\n'
 	)"
-	softwareupdate -i "$prod" --verbose
-	local ret=$?
+	softwareupdate -i "${prod}" --verbose
+	ret=$?
 
 	if [ $ret -ne 0 ]; then
 		exit_if $ret "Couldn't install ${TOPIC}!"
@@ -53,10 +49,10 @@ fi
 
 # Install Homebrew:
 TOPIC="Homebrew"
-HOMEBREW=$(command -v brew)
+HOMEBREW="$(command -v brew)"
 
-if [ -z "$HOMEBREW" ]; then
-	echo "Installing $TOPIC..."
+if [ -z "${HOMEBREW}" ]; then
+	echo "Installing ${TOPIC}..."
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 	ret=$?
 
@@ -73,9 +69,9 @@ if [[ $ret -ne 0 && "${IGNORE_BREW_DOCTOR}" -ne 1 ]]; then
 	exit_if $ret "\`brew doctor\` reported errors/warnings - fix before proceeding or set IGNORE_BREW_DOCTOR=1"
 fi
 
-# Install Homebrew packages:
+# Tap `brew bundle` and install Homebrew packages:
 TOPIC="Homebrew packages and casks"
-echo "Installing $TOPIC..."
+echo "Installing ${TOPIC}..."
 brew bundle --file=base.Brewfile
 ret=$?
 
@@ -83,9 +79,10 @@ if [ $ret -ne 0 ]; then
 	exit_if $ret "Couldn't install ${TOPIC}!"
 fi
 
-TOPIC="Extra packages and casks"
-echo "Installing $TOPIC..."
-if [ -n "$BREW_INSTALL_EXTRAS" ]; then
+# Install extra casks and packages, if specified:
+if [ "${BREW_INSTALL_EXTRAS}" -ne 1 ]; then
+	TOPIC="Extra packages and casks"
+	echo "Installing ${TOPIC}..."
 	brew bundle --file=extras.Brewfile
 	ret=$?
 
@@ -94,15 +91,18 @@ if [ -n "$BREW_INSTALL_EXTRAS" ]; then
 	fi
 fi
 
-# zsh pure prompt:
+# Install zsh pure prompt:
 PURE_DIR="/usr/local/share/zsh-pure"
-TOPIC="Pure prompt"
-echo "Installing $TOPIC..."
 
-if [[ ! -d "$PURE_DIR" ]]; then
-	git clone "https://github.com/sindresorhus/pure" "/usr/local/share/zsh-pure" \
-		&& ln -s "/usr/local/share/zsh-pure/pure.zsh" "/usr/local/share/zsh/site-functions/prompt_pure_setup" \
-		&& ln -s "/usr/local/share/zsh-pure/async.zsh" "/usr/local/share/zsh/site-functions/async"
+if [[ ! -d "${PURE_DIR}" ]]; then
+	TOPIC="zsh pure prompt"
+	echo "Installing ${TOPIC}..."
+
+	git clone "https://github.com/sindresorhus/pure" "${PURE_DIR}" \
+		&& ln -s "${PURE_DIR}/pure.zsh" \
+			"/usr/local/share/zsh/site-functions/prompt_pure_setup" \
+		&& ln -s "${PURE_DIR}/async.zsh" \
+			"/usr/local/share/zsh/site-functions/async"
 	ret=$?
 
 	if [ $ret -ne 0 ]; then
