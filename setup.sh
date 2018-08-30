f0b2bdeb23909f4200796e2b2d0fd213026f422b
Nick Miller <nick@nmiller.info>
2018-08-30 01:00:29 -0400

    Add `exit_if` function

--- setup.sh
+++ setup.sh
@@ -3,6 +3,15 @@
 ARGC=$#
 ARGV=$*
 OS="$(uname -a | xargs -n1 | head -n1)"
+# Exit with a message if $1 is nonzero.
+# $1: error code
+# $2: message
+function exit_if() {
+	if [ "${1}" -ne 0 ]; then
+		echo "${2}" >&2
+		exit "${1}"
+	fi
+}
 
 # Ensure SIGINT exits entire script instead of the current task:
 trap exit SIGINT
@@ -38,8 +47,7 @@ if [ $? -eq 1 ]; then
 	local ret=$?
 
 	if [ $ret -ne 0 ]; then
-		echo "Couldn't install $TOPIC! ($ret)" >&2
-		exit $ret
+		exit_if $ret "Couldn't install ${TOPIC}!"
 	fi
 fi
 
@@ -50,32 +58,40 @@ HOMEBREW=$(command -v brew)
 if [ -z "$HOMEBREW" ]; then
 	echo "Installing $TOPIC..."
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-	local ret=$?
+	ret=$?
 
-	if [ $? -ne 0 ]; then
-		echo "Couldn't install $TOPIC! ($ret)" >&2
-		exit $ret
+	if [ $ret -ne 0 ]; then
+		exit_if $ret "Couldn't install ${TOPIC}!"
 	fi
 fi
 
 # Ensure `brew doctor` doesn't report anything:
 brew doctor
+ret=$?
 
-if [[ "$?" -ne 0 && "$IGNORE_BREW_DOCTOR" -ne 1 ]]; then
-	echo "\`brew doctor\` reported errors/warnings - fix before proceeding"
-	echo "(or set IGNORE_BREW_DOCTOR=1 and re-run)"
-	exit 1
+if [[ $ret -ne 0 && "${IGNORE_BREW_DOCTOR}" -ne 1 ]]; then
+	exit_if $ret "\`brew doctor\` reported errors/warnings - fix before proceeding or set IGNORE_BREW_DOCTOR=1"
 fi
 
 # Install Homebrew packages:
 TOPIC="Homebrew packages and casks"
 echo "Installing $TOPIC..."
 brew bundle --file=base.Brewfile
+ret=$?
+
+if [ $ret -ne 0 ]; then
+	exit_if $ret "Couldn't install ${TOPIC}!"
+fi
 
 TOPIC="Extra packages and casks"
 echo "Installing $TOPIC..."
 if [ -n "$BREW_INSTALL_EXTRAS" ]; then
 	brew bundle --file=extras.Brewfile
+	ret=$?
+
+	if [ $ret -ne 0 ]; then
+		exit_if $ret "Couldn't install ${TOPIC}!"
+	fi
 fi
 
 # zsh pure prompt:
@@ -87,6 +103,11 @@ if [[ ! -d "$PURE_DIR" ]]; then
 	git clone "https://github.com/sindresorhus/pure" "/usr/local/share/zsh-pure" \
 		&& ln -s "/usr/local/share/zsh-pure/pure.zsh" "/usr/local/share/zsh/site-functions/prompt_pure_setup" \
 		&& ln -s "/usr/local/share/zsh-pure/async.zsh" "/usr/local/share/zsh/site-functions/async"
+	ret=$?
+
+	if [ $ret -ne 0 ]; then
+		exit_if $ret "Couldn't install ${TOPIC}!"
+	fi
 fi
 
 # Storage symlinks:
